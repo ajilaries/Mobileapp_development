f01b40f56bc1da0abc4f0dcb8f6adebda6d2bc65
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-06 19:53:17 +0530

    Code gives to define a composable funciton by using @Composable

---
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            MessageCard("Android")
+        }
+    }
+}
+@Composable
+fun MessageCard(name:String){
+    Text(text = "Hello $name")
+}
+
+//here the @Composable is used to make the composable function and the annotation is used to make the function composed the function which is used as the compose which functions like which converts the code in to ui
