00760e3249e5b1b2c40a5c07319ca528992cdc09
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-06 20:00:32 +0530

    Jetpack compose practice code with composable function and the preview

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -22,4 +23,12 @@ fun MessageCard(name:String){
     Text(text = "Hello $name")
 }
 
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard("Android")
+}
+
+//@Preview annotation lets to preview the composable functions with android studio without having to build and install the app to and android device
 //here the @Composable is used to make the composable function and the annotation is used to make the function composed the function which is used as the compose which functions like which converts the code in to ui
