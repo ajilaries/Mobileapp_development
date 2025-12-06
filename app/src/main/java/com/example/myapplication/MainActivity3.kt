4b22e524917dc25220c29221cc5b86d8c0ce564f
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-06 20:18:25 +0530

    Jetpack compose practice code with composable function and the preview and the Column ,row and box function for positioning the message card card is used to position it in vertically and the row is used to position in horizontaly and the box is used to stack elements

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,8 +46,11 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: com.example.myapplication.Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+
 }
 
 @Preview
@@ -57,3 +61,5 @@ fun PreviewMessageCard() {
     )
 }
 //lesson 2 layouts
+//column functions lets to arrange elements vertically add column to the messagecard function
+//        can use row to arrange items horizontally and box to stack element
