7446965b7ec540b975a541a114a02a917e0d734c
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-06 20:12:53 +0530

    Jetpack compose practice code with composable function and the preview

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -1,34 +1,59 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity3 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent{
-            MessageCard("Android")
-        }
-    }
-}
-@Composable
-fun MessageCard(name:String){
-    Text(text = "Hello $name")
-}
-
-
-@Preview
-@Composable
-fun PreviewMessageCard(){
-    MessageCard("Android")
-}
+//class MainActivity3 : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent{
+//            MessageCard("Android")
+//        }
+//    }
+//}
+//@Composable
+//fun MessageCard(name:String){
+//    Text(text = "Hello $name")
+//}
+//
+//
+//@Preview
+//@Composable
+//fun PreviewMessageCard(){
+//    MessageCard("Android")
+//}
 
 //@Preview annotation lets to preview the composable functions with android studio without having to build and install the app to and android device
 //here the @Composable is used to make the composable function and the annotation is used to make the function composed the function which is used as the compose which functions like which converts the code in to ui
+
+// ...
+
+class MainActivity3 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: com.example.myapplication.Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+}
+//lesson 2 layouts
