f7da7ca74112bddc24955b7df56fa6e6747b35f8
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-06 20:33:27 +0530

    Jetpack compose practice code with composable function and the preview and the Column ,row and box function for positioning the message card card is used to position it in vertically and the row is used to position in horizontally and the box is used to stack elements image adding

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 //class MainActivity3 : AppCompatActivity() {
@@ -33,33 +39,49 @@ import androidx.compose.ui.tooling.preview.Preview
 
 // ...
 
-class MainActivity3 : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
-        }
-    }
-}
+//class MainActivity3 : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MessageCard(Message("Android", "Jetpack Compose"))
+//        }
+//    }
+//}
 
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(msg: com.example.myapplication.Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
-    }
-
-}
-
-@Preview
-@Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-    )
-}
+//data class Message(val author: String, val body: String)
+//
+//@Composable
+//fun MessageCard(msg: com.example.myapplication.Message) {
+//    Column {
+//        Text(text = msg.author)
+//        Text(text = msg.body)
+//    }
+//
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewMessageCard() {
+//    MessageCard(
+//        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+//    )
+//}
 //lesson 2 layouts
 //column functions lets to arrange elements vertically add column to the messagecard function
 //        can use row to arrange items horizontally and box to stack element
+
+//
+
+//@Composable
+//fun MessageCard(msg: Message){
+//    Keyboard.Row(
+//        Image(
+//            painter = painterResource(R.drawable.ic_launcher_background),
+//            contentDescription = "Image added for test"
+//        )
+//                Column (
+//                Text(text = msg.author)
+//                        Text (text = msg.body)
+//                )
+//    )
+//}
