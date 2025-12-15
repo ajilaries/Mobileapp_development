da176f770d150f0c2088f8ebf365067cc421fb91
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-15 20:42:21 +0530

    Jetpack compose practice code with composable function and the preview and the Column ,row and box function for positioning the message card card is used to position it in vertically and the row is used to position in horizontally and the box is used to stack elements image adding

---
+++ app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.DialogFragment
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main4)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+//
+//Dialog
+//         is a class is the base class for dialogs,but donts instantiate Dialog directly instead use one of the following subcasses
+//        Create a dialog fragment
+
+class StartGameDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction.
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Start game")
+                .setPositiveButton("Start") { dialog, id ->
+                    // START THE GAME!
+                }
+                .setNegativeButton("Cancel") { dialog, id ->
+                    // User cancelled the dialog.
+                }
+            // Create the AlertDialog object and return it.
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
+
+class OldXmlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_old_xml)
+
+        StartGameDialogFragment().show(supportFragmentManager, "GAME_DIALOG")
+    }
+}
